0340f3cdc39b3ec9fa008b01bae9d74029ea2cef
Hari <hari.n@treniq.com>
2019-03-05 16:00:02 +0530

    set timing for git push to cancel

--- current_branch.rb
+++ current_branch.rb
@@ -7,12 +7,11 @@ class CurrentBranch
   end
 
   def name
-    ref = `command git symbolic-ref --quiet HEAD 2> /dev/null`
-    ret = `$?`.to_i
-    if ret.zero? || ret == 128
-      ref = `command git rev-parse --short HEAD 2> /dev/null`
+    ref = `command git symbolic-ref --quiet HEAD 2> /dev/null`.strip
+    ret = $?.exitstatus
+    unless ret.zero? || ret != 128
+      ref = `command git rev-parse --short HEAD 2> /dev/null`.strip
     end
-    binding.pry
-    `#{ref}#refs/heads/`
+    ref.gsub(/^refs\/heads\//, '')
   end
 end
